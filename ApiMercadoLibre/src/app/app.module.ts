0db054da504892eea4b64b9fb18c7342073a8114
Alejo9806 <mello0616@gmail.com>
2020-10-29 13:41:18 -0500

    Django Decimales a romanos y otras cositas que no habia subido

--- ApiMercadoLibre/src/app/app.module.ts
+++ ApiMercadoLibre/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './components/app/app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ShowComponent } from './components/show/show.component';
